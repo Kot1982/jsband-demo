4630908de9971dfe0203517b52cb8b8110400f27
color <95304946+HermanMazur@users.noreply.github.com>
2022-02-20 02:33:57 +0200

    11

--- src/slider.js
+++ src/slider.js
@@ -1,11 +1,38 @@
+<<<<<<< Updated upstream
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   autoplay: {
           delay: 2500,
           disableOnInteraction: false,
         },
+=======
+// var swiper = new Swiper(".mySwiper", {
+//   loop: true,
+//   autoplay: {
+//           delay: 2500,
+//           disableOnInteraction: false,
+//         },
+//         pagination: {
+//           el: ".swiper-pagination",
+//           clickable: true,
+//         },
+//       });
+
+var swiper = new Swiper(".mySwiper", {
+        sliderPerView: 3,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        loop: true,
+>>>>>>> Stashed changes
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
         },
+<<<<<<< Updated upstream
       });
+=======
+      });
+    
+>>>>>>> Stashed changes
